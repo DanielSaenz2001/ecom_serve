3d3abccaa0bc66d6955db9ac7a4d8d78774d1848
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2019-11-07 11:12:12 -0500

    d

--- routes/api.php
+++ routes/api.php
@@ -38,3 +38,12 @@ Route::put('persona/{id}', 'PersonaController@update');
 Route::delete('persona/{id}', 'PersonaController@destroy');
 
 
+Route::group([
+    'middleware' => 'api',
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    
+});
